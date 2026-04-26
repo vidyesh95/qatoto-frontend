8e4a3c303272552275d326c91214be9beadfad3f
Vidyesh Churi <vidyesh95@gmail.com>
2026-04-26 23:57:58 +0530

    feat: add email sign-in button and sign-up link to authentication page with updated UI styling

--- src/components/home/sign-in.tsx
+++ src/components/home/sign-in.tsx
@@ -4,7 +4,7 @@ export default function SignIn() {
   return (
     <main className="min-h-screen w-screen flex flex-col">
       <header className="bg-background">
-        <div className="px-4 py-5">
+        <div className="px-4 py-5 cursor-pointer">
           <Image
             src="/icons/arrow_back_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg"
             alt="Navigate back"
@@ -33,11 +33,24 @@ export default function SignIn() {
           />
           <span>Continue with Apple</span>
         </button>
-        <div className="px-4 flex items-center gap-4 text-muted-foreground">
+        <div className="px-4 flex items-center gap-4 text-[#BEC8C9]">
           <hr className="flex-1"/>
           <span className="text-xs">or</span>
           <hr className="flex-1"/>
         </div>
+        <button type={"button"} className={"w-full justify-center items-center text-sm font-medium bg-[#00696E] text-background flex gap-2 border border-outline rounded-full pl-4 pr-6 py-2.5 cursor-pointer"}>
+          <Image
+            src={"/icons/mail_18dp_FFFFFF_FILL1_wght400_GRAD0_opsz20.svg"}
+            alt={"Sign in with Password"}
+            width={18}
+            height={18}
+          />
+          <span>Sign in with Password</span>
+        </button>
+        <p className="text-sm font-medium text-center space-x-1">
+          <span className="text-[#BEC8C9]">Don't have an account?</span>
+          <span className="text-[#00696E] cursor-pointer">Sign up</span>
+        </p>
       </section>
     </main>
   )
