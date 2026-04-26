ecf4b2722e834b60c25003df6153be4ae360db05
Vidyesh Churi <vidyesh95@gmail.com>
2026-04-26 21:50:28 +0530

    feat: add Apple sign-in option and layout styling to sign-in component

--- src/components/home/sign-in.tsx
+++ src/components/home/sign-in.tsx
@@ -14,8 +14,8 @@ export default function SignIn() {
         </div>
         <p className="px-5 pt-5 pb-4 text-3xl">Sign in</p>
       </header>
-      <section>
-        <button type={"button"} className={"text-sm text-[#00696E] flex gap-2 border border-outline rounded-full pl-4 pr-6 py-2.5 cursor-pointer"}>
+      <section className="p-4 space-y-4">
+        <button type={"button"} className={"w-full justify-center text-sm font-medium text-[#00696E] flex gap-2 border border-outline rounded-full pl-4 pr-6 py-2.5 cursor-pointer"}>
           <Image
             src={"/icons/google_logo_18x18px.svg"}
             alt={"Continue with Google"}
@@ -24,6 +24,20 @@ export default function SignIn() {
           />
           <span>Continue with Google</span>
         </button>
+        <button type={"button"} className={"w-full justify-center text-sm font-medium text-[#00696E] flex gap-2 border border-outline rounded-full pl-4 pr-6 py-2.5 cursor-pointer"}>
+          <Image
+            src={"/icons/apple_logo_18x18px.svg"}
+            alt={"Continue with Apple"}
+            width={18}
+            height={18}
+          />
+          <span>Continue with Apple</span>
+        </button>
+        <div className="p-4 flex items-center gap-4 text-muted-foreground">
+          <hr className="flex-1"/>
+          <span className="text-xs">or</span>
+          <hr className="flex-1"/>
+        </div>
       </section>
     </main>
   )
