b20f15330c69f7604ad09ef0dd1ea39b2e905748
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-04 16:31:02 +0530

    refactor: remove unnecessary fragment wrapper from home layout component

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -12,13 +12,11 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <SidebarProvider>
-      <>
-        <Navbar />
-        <div className="flex">
-          <Sidebar />
-          <main className="flex-1">{children}</main>
-        </div>
-      </>
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1">{children}</main>
+      </div>
     </SidebarProvider>
   );
 };
