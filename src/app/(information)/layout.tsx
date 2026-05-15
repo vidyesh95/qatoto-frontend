620824d5f5e2f58e451bea4ecbc1d91c44fb961e
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-15 15:14:31 +0530

    feat: common nav for home navigation for information pages

---
+++ src/app/(information)/layout.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <nav className="sticky top-0 z-50 bg-background">
+        <div className="px-4 md:px-16.5 py-2.5">
+          <Link href="/" className="text-3xl font-serif font-medium text-[#00696E]">
+            Qatoto
+          </Link>
+        </div>
+      </nav>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
