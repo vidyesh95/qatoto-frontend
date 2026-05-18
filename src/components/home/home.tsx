5163f8bdb595a09ce1f67301028cf506d88991a6
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-18 11:51:17 +0530

    feat: added promo banner

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main>home</main>;
+  return (
+    <main>
+      <div className="relative w-full flex justify-center bg-gray-200" id="promo-carousel">
+        <Image
+          src={"/dummy/highlight_image01.avif"}
+          width={1080}
+          height={260}
+          alt="Advertise dummy image 1"
+        />
+        <button className="absolute top-0 right-0 p-2">
+          <Image
+            src={"/icons/close_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg"}
+            width={24}
+            height={24}
+            alt="close promo"
+          />
+        </button>
+      </div>
+    </main>
+  );
 }
