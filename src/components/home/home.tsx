bac4a1d503d79796bd862b08e7d0ac9262828db5
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-19 23:18:25 +0530

    feat:chip filter

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -7,10 +7,10 @@ export default function Home() {
       {/* <PromoCarousel /> */}
 
       {/* Filter */}
-      <div>
+      <div className="relative">
         <button
           type="button"
-          className="bg-linear-to-r from-white via-white to-transparent py-4 pl-4 pr-18 cursor-pointer"
+          className="absolute left-0 top-0 bottom-0 bg-linear-to-r from-white via-white to-transparent py-4 pl-4 pr-18 cursor-pointer"
         >
           <Image
             src="/icons/chevron_backward_24dp_000000_FILL1_wght400_GRAD0_opsz24.svg"
@@ -19,9 +19,29 @@ export default function Home() {
             alt="Navigate filter back"
           />
         </button>
+        <div className="h-14 flex flex-row items-center gap-2">
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Live</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Trending</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">New to you</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">News</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Recently uploaded</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Watched</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Gaming</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Shopping</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Cosplay</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Music</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">News</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Calming</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Cooking</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Travel</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Sports</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Cars</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Education</div>
+          <div className="px-4 py-1.5 text-sm text-nowrap border border-outline rounded-md">Science and technology</div>
+        </div>
         <button
           type="button"
-          className="bg-linear-to-l from-white via-white to-transparent py-4 pl-18 pr-4 cursor-pointer"
+          className="absolute right-0 top-0 bottom-0 bg-linear-to-l from-white via-white to-transparent py-4 pl-18 pr-4 cursor-pointer"
         >
           <Image
             src="/icons/chevron_forward_24dp_000000_FILL1_wght400_GRAD0_opsz24.svg"
