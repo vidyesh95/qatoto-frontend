d9f24335d39d474474f4ff7e4c671504696f111c
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-19 22:14:23 +0530

    feat: chip filter

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,30 @@
 import PromoCarousel from "@/components/home/promo-carousel";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <PromoCarousel />
+      {/* <PromoCarousel /> */}
+
+      {/* Filter */}
+      <div>
+        <button type="button" className="bg-linear-to-r from-white via-white to-transparent py-4 pl-4 pr-18">
+          <Image
+            src="/icons/chevron_backward_24dp_000000_FILL1_wght400_GRAD0_opsz24.svg"
+            width={24}
+            height={24}
+            alt="Navigate filter back"
+          />
+        </button>
+        <button type="button" className="bg-linear-to-l from-white via-white to-transparent py-4 pl-18 pr-4">
+          <Image
+            src="/icons/chevron_forward_24dp_000000_FILL1_wght400_GRAD0_opsz24.svg"
+            width={24}
+            height={24}
+            alt="Navigate filter forward"
+          />
+        </button>
+      </div>
     </main>
   );
 }
