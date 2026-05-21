81da1c9821df854b07e751245d22e5f00819aadc
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-21 22:32:36 +0530

    feat: filter info

--- src/components/home/filter.tsx
+++ src/components/home/filter.tsx
@@ -196,9 +196,8 @@ export default function Filter() {
               key={`${chipLabel}-${chipIndex}`}
               type="button"
               onClick={() => setSelectedChipIndex(chipIndex)}
-              className={`px-4 py-1.5 text-sm text-nowrap rounded-lg cursor-pointer border ${
-                isSelected ? "bg-primary border-primary" : "border-outline hover:bg-black/5"
-              }`}
+              className={`px-4 py-1.5 text-sm text-nowrap rounded-lg cursor-pointer border ${isSelected ? "bg-primary border-primary" : "border-outline hover:bg-black/5"
+                }`}
             >
               {chipLabel}
             </button>
