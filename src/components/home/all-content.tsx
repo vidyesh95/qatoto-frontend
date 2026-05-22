6bc53dab2b485ccb381e68f71862041f2311e6bf
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-22 11:16:08 +0530

    feat: all content divider

--- src/components/home/all-content.tsx
+++ src/components/home/all-content.tsx
@@ -2,12 +2,12 @@ export default function AllContent() {
   return (
     <section className="py-14 space-y-14">
       <div>
-        <div className="flex flex-row">
-          <hr />
-          <span className="text-[#5C5F5F] text-2xl font-medium tracking-wider">
+        <div className="flex flex-row items-center">
+          <hr className="flex-1 border-t border-[#A9ACAC] mx-6" />
+          <span className="text-[#5C5F5F] text-2xl font-medium tracking-wider whitespace-nowrap">
             RECOMMENDED FOR YOU
           </span>
-          <hr />
+          <hr className="flex-1 border-t border-[#A9ACAC] mx-6" />
         </div>
       </div>
     </section>
