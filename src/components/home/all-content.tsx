75e85348cf31f93da0c7d32cf552a4f400a4c7e9
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-23 14:57:06 +0530

    feat: thumnail📌

--- src/components/home/all-content.tsx
+++ src/components/home/all-content.tsx
@@ -1,10 +1,18 @@
 import SectionDivider from "@/components/home/section-divider";
+import Image from "next/image";
 
 export default function AllContent() {
   return (
     <section className="py-14 space-y-14">
       <div>
         <SectionDivider title="RECOMMENDED FOR YOU" />
+        <Image
+          src={"/dummy/thumbnail_image01.avif"}
+          width={246}
+          height={138}
+          alt="thumbnail"
+          className="rounded-xl"
+        />
       </div>
     </section>
   );
