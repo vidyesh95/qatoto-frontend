e07284e6d9b5c18411da6cb28be1282f3a3a5b0a
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-24 17:24:06 +0530

    feat: video enhance

--- src/components/home/all-content.tsx
+++ src/components/home/all-content.tsx
@@ -6,15 +6,86 @@ export default function AllContent() {
     <section className="py-14 space-y-14">
       <div>
         <SectionDivider title="RECOMMENDED FOR YOU" />
-        <div className="">
+        <div className="flex flex-row">
           <VideoCard
             thumbnailSrc="/dummy/thumbnail_image01.avif"
             profileSrc="/dummy/profile_image_01.avif"
             title="Pomporo singing 🌼Fengzhi Senai🌼 at Disney Land"
             channelName="Arin Light"
-            views="2.5M"
+            views="2.5M views"
             postedAt="12 hours ago"
             verified
+            hoverBg="hover:bg-yellow-100"
+          />
+          <VideoCard
+            thumbnailSrc="/dummy/thumbnail_image02.avif"
+            profileSrc="/dummy/profile_image_02.avif"
+            title="Need for speed @234MPH"
+            channelName="BTS fan boi🤩"
+            views="973 views"
+            postedAt="37 minutes ago"
+            verified
+            hoverBg="hover:bg-amber-100"
+          />
+          <VideoCard
+            thumbnailSrc="/dummy/thumbnail_image03.avif"
+            profileSrc="/dummy/profile_image_03.avif"
+            title="Your everyday slicing made easy - cucumber, carrot, radish in snap"
+            channelName="Home owners friend"
+            views="9k watching"
+            postedAt="Live"
+            verified
+            hoverBg="hover:bg-green-100"
+          />
+          <VideoCard
+            thumbnailSrc="/dummy/thumbnail_image04.avif"
+            profileSrc="/dummy/profile_image_04.avif"
+            title="Driving on Tour de Auto Weisseir"
+            channelName="BMW Girl"
+            views="275K watching"
+            postedAt="Live"
+            verified
+            hoverBg="hover:bg-green-100"
+          />
+          <VideoCard
+            thumbnailSrc="/dummy/thumbnail_image05.avif"
+            profileSrc="/dummy/profile_image_05.avif"
+            title="Traditional Mongolian luxury villa"
+            channelName="Luxury Investment"
+            views="189K views"
+            postedAt="17 hours ago"
+            verified
+            hoverBg="hover:bg-yellow-100"
+          />
+          <VideoCard
+            thumbnailSrc="/dummy/thumbnail_image06.avif"
+            profileSrc="/dummy/profile_image_06.avif"
+            title="Катюша танец"
+            channelName="товарищ Сталин"
+            views="7.9M views"
+            postedAt="3 weeks ago"
+            verified
+            hoverBg="hover:bg-red-100"
+          />
+          <VideoCard
+            thumbnailSrc="/dummy/thumbnail_image07.avif"
+            profileSrc="/dummy/profile_image_07.avif"
+            title="Studio Ghibli anime My Neighbour Totoro"
+            channelName="Little Red Riding Hood"
+            views="1.3K views"
+            postedAt="3 hours ago"
+            verified
+            hoverBg="hover:bg-amber-100"
+          />
+          <VideoCard
+            thumbnailSrc="/dummy/thumbnail_image08.avif"
+            profileSrc="/dummy/profile_image_08.avif"
+            title="Buy Smart LED Wifi dress for night driving"
+            channelName="Ecoco"
+            views="57M views"
+            postedAt="2 years ago"
+            verified
+            hoverBg="hover:bg-blue-100"
           />
         </div>
       </div>
--- src/components/home/video-card.tsx
+++ src/components/home/video-card.tsx
@@ -46,7 +46,7 @@ export default function VideoCard({
             )}
           </div>
           <div className="flex flex-row items-center gap-1">
-            <span className="text-xs text-[#6F7979]">{views} views</span>
+            <span className="text-xs text-[#6F7979]">{views}</span>
             <Image
               src={"/icons/circle_24dp_6F7979_FILL1_wght400_GRAD0_opsz24.svg"}
               width={4}
