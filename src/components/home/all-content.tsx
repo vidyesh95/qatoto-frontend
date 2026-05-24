2f1db8c66857c60f2aa537a31231d636287177b9
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-24 17:49:12 +0530

    fix: video card fix

--- src/components/home/all-content.tsx
+++ src/components/home/all-content.tsx
@@ -99,7 +99,7 @@ export default function AllContent() {
     <section className="py-14 space-y-14">
       <div>
         <SectionDivider title="RECOMMENDED FOR YOU" />
-        <div className="flex flex-row">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 px-4">
           {RECOMMENDED_VIDEOS.map(({ id, ...video }) => (
             <VideoCard key={id} {...video} />
           ))}
