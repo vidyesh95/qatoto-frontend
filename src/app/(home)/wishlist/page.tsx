f68f5496cb703d1589b9100554f96f5bdf656479
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-31 13:40:38 +0530

    feat: wishlist page

---
+++ src/app/(home)/wishlist/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Wishlist | Qatoto",
+  description: "Wishlist page for Qatoto",
+};
+
+export default function Wishlist() {
+  return <h1>Wishlist</h1>;
+}
