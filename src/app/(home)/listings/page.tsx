f9190c3a9e56b0e57ef08ca9f70af4755cee56ce
Vidyesh Churi <vidyesh95@gmail.com>
2026-05-31 13:44:51 +0530

    fix: removed your

---
+++ src/app/(home)/listings/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Listings | Qatoto",
+  description: "Listings page for Qatoto",
+};
+
+export default function Listings() {
+  return <h1>Listings</h1>;
+}
---
+++ src/app/(home)/sales/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sales | Qatoto",
+  description: "Sales page for Qatoto",
+};
+
+export default function Sales() {
+  return <h1>Sales</h1>;
+}
