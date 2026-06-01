0ed0bee078e1197b3221b6bf72b247e3999144c7
Vidyesh Churi <vidyesh95@gmail.com>
2026-06-01 20:08:04 +0530

    fix: mobile layout center and divider hidden

--- src/components/home/section-divider.tsx
+++ src/components/home/section-divider.tsx
@@ -5,11 +5,11 @@ interface SectionDividerProps {
 export default function SectionDivider({ title }: SectionDividerProps) {
   return (
     <div className="flex flex-row items-center">
-      <hr className="flex-1 border-t border-[#A9ACAC] mx-6" />
-      <h2 className="text-[#5C5F5F] text-2xl font-medium tracking-wider whitespace-nowrap">
+      <hr className="hidden md:flex flex-1 border-t border-[#A9ACAC] mx-6" />
+      <h2 className="mx-auto text-[#5C5F5F] text-2xl font-medium tracking-wider whitespace-nowrap">
         {title}
       </h2>
-      <hr className="flex-1 border-t border-[#A9ACAC] mx-6" />
+      <hr className="hidden md:flex flex-1 border-t border-[#A9ACAC] mx-6" />
     </div>
   );
 }
